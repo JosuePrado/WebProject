df68a5d2ecd6af27ad804e7c4faef1f8146d1963
Josue_prado <prado69508698@gmial.com>
2024-09-25 13:14:56 -0400

    feat: implement first version of websocket server

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Fleck;
+
+var server = new WebSocketServer("ws://127.0.0.1:8181");
+
+var wsClientsConnected = new List<IWebSocketConnection>();
+
+server.Start(wsConnection =>
+{
+    wsConnection.OnOpen = () =>
+    {
+        wsClientsConnected.Add(wsConnection);
+    };
+
+    wsConnection.OnMessage = message =>
+    {
+        wsClientsConnected.ForEach(client =>
+        {
+            if (client != wsConnection)
+            {
+                client.Send(message);
+            }
+
+        });
+    };
+});
+
+WebApplication.CreateBuilder(args).Build().Run();
