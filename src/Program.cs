5b6708c66bc227e7bca9286d89d656f212ceb275
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2024-09-27 08:48:11 -0400

    feat: add custom channel and default channel functionality

--- src/Program.cs
+++ src/Program.cs
@@ -1,26 +1,65 @@
 ﻿using Fleck;
+using System;
+using System.Collections.Generic;
 
 var server = new WebSocketServer("ws://127.0.0.1:8181");
 
-var wsClientsConnected = new List<IWebSocketConnection>();
+var channelClients = new Dictionary<string, List<IWebSocketConnection>>();
 
 server.Start(wsConnection =>
 {
     wsConnection.OnOpen = () =>
     {
-        wsClientsConnected.Add(wsConnection);
+        var query = wsConnection.ConnectionInfo.Path;
+        var channel = query.TrimStart('?');
+
+        if (string.IsNullOrEmpty(channel))
+        {
+            channel = "default";
+        }
+
+        if (!channelClients.ContainsKey(channel))
+        {
+            channelClients[channel] = new List<IWebSocketConnection>();
+        }
+        channelClients[channel].Add(wsConnection);
+
+        Console.WriteLine($"Cliente conectado al canal: {channel}");
+    };
+
+    wsConnection.OnClose = () =>
+    {
+        foreach (var channel in channelClients.Keys)
+        {
+            if (channelClients[channel].Contains(wsConnection))
+            {
+                channelClients[channel].Remove(wsConnection);
+                Console.WriteLine($"Cliente desconectado del canal: {channel}");
+                break;
+            }
+        }
     };
 
     wsConnection.OnMessage = message =>
     {
-        wsClientsConnected.ForEach(client =>
-        {
-            if (client != wsConnection)
-            {
-                client.Send(message);
-            }
+        var query = wsConnection.ConnectionInfo.Path;
+        var channel = query.TrimStart('?');
 
-        });
+        if (string.IsNullOrEmpty(channel))
+        {
+            channel = "default";
+        }
+
+        if (channelClients.ContainsKey(channel))
+        {
+            channelClients[channel].ForEach(client =>
+            {
+                if (client != wsConnection)
+                {
+                    client.Send(message);
+                }
+            });
+        }
     };
 });
 
